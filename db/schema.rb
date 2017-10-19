f61c2151ccbab6c2251760696e64c7136eb31e1f
Scott Mandrell <sdmandrell@berkeley.edu>
2017-10-19 05:29:08 +0000

    got app to deploy locally and created restaurant route

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171019045909) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
---
+++ spec/views/restaurants/index.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "restaurants/index", type: :view do
+  before(:each) do
+    assign(:restaurants, [
+      Restaurant.create!(
+        :name => "Name",
+        :description => "MyText"
+      ),
+      Restaurant.create!(
+        :name => "Name",
+        :description => "MyText"
+      )
+    ])
+  end
+
+  it "renders a list of restaurants" do
+    render
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+  end
+end

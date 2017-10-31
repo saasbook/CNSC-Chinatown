32f1386f338e54c65f918f59460dde57938c93f4
Ebrahem Farooqui <ebe.farooqui@berkeley.edu>
2017-10-31 21:22:54 +0000

    Filter features

--- features/step_definitions/filter_steps.rb
+++ features/step_definitions/filter_steps.rb
@@ -15,3 +15,13 @@ end
 Given /^(?:|I )am on (.+)$/ do |page_name|
   visit path_to(page_name)
 end
+
+Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
+  regexp = Regexp.new(regexp)
+
+  if page.respond_to? :should
+    page.should have_xpath('//*', :text => regexp)
+  else
+    assert page.has_xpath?('//*', :text => regexp)
+  end
+end
